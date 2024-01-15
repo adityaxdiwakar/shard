4035b47a715c17ee0b7bc85e2a09349bd707d931
Aditya Diwakar <aditya@diwakar.io>
2024-01-15 14:14:39 +0530

    templatize all directions and books

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,29 +1,29 @@
+#include <unordered_map>
 #include <stdint.h>
 #include <string>
 #include <memory>
-#include <queue>
-#include <unordered_map>
+#include <queue> 
 #include <set>
+    
+enum class Direction { BUY, SELL };
 
+template<Direction dir>
 struct Order {
-    enum Direction { BUY, SELL };
 
     std::string participant; // TODO: use user order id, not strig
     std::string symbol; // TODO: use symbol id
     uint32_t order_id;
     uint32_t price; // in number of pennies (or per underlying)
     uint32_t size;
-    Direction dir;
     uint32_t ts;
 
     bool valid = true;
 
-    Order(std::string participant, Direction dir, std::string symbol, uint32_t price, uint32_t size)
+    Order(std::string participant, std::string symbol, uint32_t price, uint32_t size)
         : participant(participant)
         , symbol(symbol)
         , price(price)
         , size(size)
-        , dir(dir)
     {}
 
     // cancel will invalidate the order, so that when the matching engine
@@ -36,49 +36,49 @@ struct Order {
     void cancel() { valid = false; }
 };
 
+template<Direction dir>
 struct Level {
-    std::queue<std::shared_ptr<Order>> orders;
+    std::queue<std::shared_ptr<Order<dir>>> orders;
     uint32_t volume;
     uint32_t price; // in number of pennies
-    Order::Direction dir;
 
-    Level(Order::Direction dir) : dir(dir) {}
-    Level(std::shared_ptr<Order> order)
+    Level(std::shared_ptr<Order<dir>> order)
         : volume(0)
         , price(order->price)
-        , dir(order->dir)
     {}
 
-    void add_order(std::shared_ptr<Order> order);
+    void add_order(std::shared_ptr<Order<dir>> order);
 
     struct Cmp {
         bool cmp_ref(const Level& a, const Level& b) const {
-            if (a.dir == Order::BUY) return a.price < b.price;
+            if constexpr (dir == Direction::BUY) return a.price < b.price;
             else return a.price > b.price;
         }
 
-        bool operator()(const std::shared_ptr<Level>& a, const std::shared_ptr<Level>& b) const {
+        bool operator()(const std::shared_ptr<Level<dir>>& a, const std::shared_ptr<Level<dir>>& b) const {
             return cmp_ref(*a, *b);
         }
     };
 };
 
-// maps price levels -> level objects
-using LevelMap = std::unordered_map<uint32_t, std::shared_ptr<Level>>;
 
-// maps order ids -> order objects
-using OrderMap = std::unordered_map<uint32_t, std::shared_ptr<Order>>;
-
-// levels book
-using LevelBook = std::set<std::shared_ptr<Level>, Level::Cmp>;
-
+template<Direction dir>
 class Book { 
-    Order::Direction dir; // 2 books will exist, bid and ask book
-
-    void add_order(std::shared_ptr<Order> order);
+    void add_order(std::shared_ptr<Order<dir>> order);
 
     private:
+        using LevelBook = std::set<std::shared_ptr<Level<dir>>, typename Level<dir>::Cmp>;
+        // maps price levels -> level objects
+        using LevelMap = std::unordered_map<uint32_t, std::shared_ptr<Level<dir>>>;
+        // maps order ids -> order objects
+        using OrderMap = std::unordered_map<uint32_t, std::shared_ptr<Order<dir>>>;
+
         OrderMap order_map;
         LevelMap level_map;
         LevelBook levels;
+};
+
+class MatchingEngine {
+    Book<Direction::BUY> bidBook;
+    Book<Direction::SELL> askBook;
 };
--- src/engine.cc
+++ src/engine.cc
@@ -1,14 +1,16 @@
 #include "engine.hpp"
 
-void Level::add_order(std::shared_ptr<Order> order) {
+template<Direction dir>
+void Level<dir>::add_order(std::shared_ptr<Order<dir>> order) {
     orders.push(order);
     volume += order->size;
 }
 
-void Book::add_order(std::shared_ptr<Order> order) {
+template<Direction dir>
+void Book<dir>::add_order(std::shared_ptr<Order<dir>> order) {
     // if not in level map, insert into level map and levels book set
     if (level_map.find(order->price) == level_map.end()) {
-        std::shared_ptr<Level> new_level = make_shared<Level>(order);
+        std::shared_ptr<Level<dir>> new_level = make_shared<Level>(order);
         level_map[order->price] = new_level;
         levels.insert(new_level);
     }
